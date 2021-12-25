f6545e4dee2ef65b5e715ee64480ed7f85421c6d
Eva van Houten <user@server.fake>
2021-12-25 14:09:50 +0100

    Extracted function get_chapter_texts().

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import ebooklib
 from ebooklib import epub
 from bs4 import BeautifulSoup
 
-book = epub.read_epub("book1.epub")
-for item in book.get_items():
-    if item.get_type() == ebooklib.ITEM_DOCUMENT:
-        name = item.get_name().lower()
-        content = item.get_content()
-        if "chapter" in name and "nextbook" not in name:
-            chapter = re.search("chapter([0-9]{2})", name)[1]
-            chapter = int(chapter)
-            text = BeautifulSoup(content).get_text()
+
+def get_chapter_texts(book_path):
+    book = epub.read_epub(book_path)
+    result = []
+    previous_chapter = None
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            name = item.get_name().lower()
+            content = item.get_content()
+            if "chapter" in name and "nextbook" not in name:
+                chapter = re.search("chapter([0-9]{2})", name)[1]
+                chapter = int(chapter)
+                print(chapter)
+                text = BeautifulSoup(content, features="lxml").get_text()
+                if previous_chapter is not None:
+                    assert (chapter == previous_chapter + 1), \
+                        (f"Previous chapter is {previous_chapter}, "
+                         f"but this chapter is {chapter}")
+                result.append(text)
+                previous_chapter = chapter
+    return result
+
+
+if __name__ == '__main__':
+    book_path = "book1.epub"
+    chapter_texts = get_chapter_texts(book_path)
