dae2bd1a226127285c3f38f8933bcf67be04471d
Eva van Houten <user@server.fake>
2021-12-28 14:18:45 +0100

    Added assertion in text_parsing __main__ as a test.

--- text_parsing.py
+++ text_parsing.py
@@ -27,3 +27,6 @@ if __name__ == '__main__':
     norwegian_nonsense = ("jeg- jeg er. t-skjortene er ikke. jeg...og du. "
                           "han-som-ser er her. a leve.")
     words = get_words(norwegian_nonsense)
+    correct_words = ['jeg', 'jeg', 'er', 't-skjortene', 'er', 'ikke', 'jeg',
+                     'og', 'du', 'han-som-ser', 'er', 'her', 'a', 'leve']
+    assert (words == correct_words), f"words: {words} != {correct_words}"
