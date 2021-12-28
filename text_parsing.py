a11feeb905080bd928c616bb5bc836c3bfd06a86
Eva van Houten <user@server.fake>
2021-12-28 14:10:59 +0100

    Added function to generate a dict of lists of all unicode letters and non-separator characters.

--- text_parsing.py
+++ text_parsing.py
@@ -1,10 +1,26 @@
 import re
+import sys
+import unicodedata
 
 
 def get_words(text):
     word_pattern = r"(?:[a-z]+(?:[\-.][a-z]+)*[a-z])|[a-z]"
     words = re.findall(word_pattern, text)
     return words
+
+
+def unicode_category_lists():
+    result = {"letters": [], "non-separators": []}
+    for i in range(sys.maxunicode):
+        category = unicodedata.category(chr(i))
+
+        if category.startswith('L'):
+            result["letters"].append(chr(i))
+
+        if not category.startswith('Z'):  # Z means separator
+            result["non-separators"].append(chr(i))
+
+    return result
 
 
 if __name__ == '__main__':
