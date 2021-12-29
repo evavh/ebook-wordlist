158412cfae1934a104d18aa8fb93e163b5a41774
Eva van Houten <user@server.fake>
2021-12-29 15:08:12 +0100

    Added parsing all words per chapter to main, updated README with assumptions about words.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # (WORK IN PROGRESS) #
 This Python script will generate lists of words per chapter with translation. Input will be a folder of foreign language epubs, output will be sorted by frequency and filtered for words already encountered in previous books and chapters.
 
+Currently only parses words from epub text by chapter, using unicode categories to distinguish letters and non-seperator characters. This means it could work with any language, assuming that a word starts and ends with a "letter", doesn't have a "seperator" or "control character" in it, and that a word does not contain two non-"letters" in a row.
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import ebooklib
 from ebooklib import epub
 from bs4 import BeautifulSoup
 
+import text_parsing
+
 
 def get_chapter_texts(book_path):
     book = epub.read_epub(book_path)
@@ -15,13 +17,15 @@ def get_chapter_texts(book_path):
             if "chapter" in name and "nextbook" not in name:
                 chapter = re.search("chapter([0-9]{2})", name)[1]
                 chapter = int(chapter)
-                print(chapter)
+
                 text = BeautifulSoup(content, features="lxml")\
                     .get_text(separator="\n")
+
                 if previous_chapter is not None:
                     assert (chapter == previous_chapter + 1), \
                         (f"Previous chapter is {previous_chapter}, "
                          f"but this chapter is {chapter}")
+
                 result.append(text)
                 previous_chapter = chapter
     return result
@@ -33,5 +37,9 @@ if __name__ == '__main__':
 
     for index, chapter_text in enumerate(chapter_texts):
         chapter_number = index + 1
+
+        L, NS = text_parsing.cache_regex_strings()
+        words = text_parsing.get_words(chapter_text.lower(), L, NS)
+
         if chapter_number == 1:
-            print(chapter_text)
+            print(words)
