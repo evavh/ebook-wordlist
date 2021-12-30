1eb66fdbab355d50976c0cafa25c863a39fe9175
Eva van Houten <user@server.fake>
2021-12-30 13:50:58 +0100

    Moved get_chapter_texts() to new file_io.py.

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import re
-import ebooklib
-from ebooklib import epub
-from bs4 import BeautifulSoup
-
+#!/usr/bin/python3
 import text_parsing
-
-
-def get_chapter_texts(book_path):
-    book = epub.read_epub(book_path)
-    result = []
-    previous_chapter = None
-    for item in book.get_items():
-        if item.get_type() == ebooklib.ITEM_DOCUMENT:
-            name = item.get_name().lower()
-            content = item.get_content()
-            if "chapter" in name and "nextbook" not in name:
-                chapter = re.search("chapter([0-9]{2})", name)[1]
-                chapter = int(chapter)
-
-                text = BeautifulSoup(content, features="lxml")\
-                    .get_text(separator="\n")
-
-                if previous_chapter is not None:
-                    assert (chapter == previous_chapter + 1), \
-                        (f"Previous chapter is {previous_chapter}, "
-                         f"but this chapter is {chapter}")
-
-                result.append(text)
-                previous_chapter = chapter
-    return result
-
+import file_io
 
 if __name__ == '__main__':
     book_path = "book1.epub"
-    chapter_texts = get_chapter_texts(book_path)
+    chapter_texts = file_io.get_chapter_texts(book_path)
 
     for index, chapter_text in enumerate(chapter_texts):
         chapter_number = index + 1
